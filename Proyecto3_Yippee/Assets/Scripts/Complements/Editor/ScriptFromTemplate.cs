d3792c160d3bad6e5c962f905aa975495b86dfdb
DarkAlejoxD <clondono@edu.tecnocampus.cat>
2024-04-08 01:22:01 +0200

    Base Template

--- Proyecto3_Yippee/Assets/Scripts/Complements/Editor/ScriptFromTemplate.cs
+++ Proyecto3_Yippee/Assets/Scripts/Complements/Editor/ScriptFromTemplate.cs
@@ -1,15 +1,17 @@
+using Unity.VisualScripting;
 using UnityEditor;
+using UnityEngine;
 
 namespace UtilsComplements
 {
     public class ScriptFromTemplate
     {
-        private const string PATH_TO_BASETEMPLATE = "";
+        private const string PATH_TO_BASETEMPLATE = "Assets/Scripts/Complements/Editor/BaseTemplate.cs.txt";
 
         [MenuItem(itemName: "Assets/Create/ScriptTemplates/C# UnityBase Template", isValidateFunction: false, priority = 22)]
         public static void CreateScriptFromTemplate_BaseTemplate()
         {
-            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(PATH_TO_BASETEMPLATE, "newScript.cs");
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(PATH_TO_BASETEMPLATE, "NewScript.cs");
         }
     }
 }
