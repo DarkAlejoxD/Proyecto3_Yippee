e7fee709c95db3b136656d21672dc8e9979be832
DarkAlejoxD <clondono@edu.tecnocampus.cat>
2024-04-08 19:09:53 +0200

    ISingleton Instance Methods

--- Proyecto3_Yippee/Assets/Scripts/Complements/ISingleton.cs
+++ Proyecto3_Yippee/Assets/Scripts/Complements/ISingleton.cs
@@ -1,4 +1,37 @@
 public interface ISingleton<T> where T : class, ISingleton<T>
 {
-    //TODO: Complete this class. Ask a teacher if recommends this style of Singleton o Generic interfaces.
+    //TODO: Complete this class. Ask a teacher if recommends this style of Singleton o Generic interfaces/classes.
+    private static T _singleton;
+
+    public ISingleton<T> Instance => this;
+    public T Value => (T)this;
+
+    #region Static Fields & Methods
+
+    #endregion
+
+    #region Instance Fields & Methods
+    public sealed void Instantiate()
+    {
+        if (_singleton == null || _singleton == default)        
+            _singleton = Value;
+        
+        else
+            Invalidate();        
+    }
+
+    public sealed void RemoveInstance()
+    {
+        if (_singleton == Value)        
+            _singleton = null;        
+    }
+
+    public void Invalidate()
+    {
+#if UNITY_2022_3_OR_NEWER
+        if(Value is UnityEngine.Component comp)        
+            UnityEngine.Component.Destroy(comp);        
+#endif
+    }
+    #endregion
 }
