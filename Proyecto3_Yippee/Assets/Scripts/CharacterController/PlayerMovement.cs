7b82b5caf7ff3d84163617e1d1cf380f748f3ff0
DarkAlejoxD <clondono@edu.tecnocampus.cat>
2024-04-08 02:08:10 +0200

    InitialTransform&ProtoSingleton

--- Proyecto3_Yippee/Assets/Scripts/CharacterController/PlayerMovement.cs
+++ Proyecto3_Yippee/Assets/Scripts/CharacterController/PlayerMovement.cs
@@ -48,3 +48,4 @@ namespace CharacterController
         #endregion
     }
 }
+
---
+++ Proyecto3_Yippee/Assets/Scripts/Complements/ISingleton.cs
@@ -0,0 +1,4 @@
+public interface ISingleton<T> where T : class, ISingleton<T>
+{
+    //TODO: Complete this class. Ask a teacher if recommends this style of Singleton o Generic interfaces.
+}
