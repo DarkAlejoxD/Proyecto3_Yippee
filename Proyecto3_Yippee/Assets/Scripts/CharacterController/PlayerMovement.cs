469e41365a0eef4a14d43e44e2bacf9214a7d165
Skfod37 <skfod37@gmail.com>
2024-04-12 18:39:13 +0200

    Update PlayerMovement.cs

--- Proyecto3_Yippee/Assets/Scripts/CharacterController/PlayerMovement.cs
+++ Proyecto3_Yippee/Assets/Scripts/CharacterController/PlayerMovement.cs
@@ -13,9 +13,14 @@ namespace AvatarController
         //TODO: Subscribe to OnMove del PlayerController
         #region Fields
 
+        private Vector3 _velocity;
+
+
         private PlayerController _playerController;
+        private CharacterController _characterController;
         private PlayerData Data => _playerController.DataContainer;
 
+        private Camera CurrentCamera => Camera.main; //TODO: Change this
         
         #endregion
 
@@ -40,11 +45,19 @@ namespace AvatarController
 
         private void Awake()
         {
+            Application.targetFrameRate = 60;
             _playerController = GetComponent<PlayerController>();
+            _characterController = GetComponent<CharacterController>();
+        }
+
+        private void Start()
+        {
+            _velocity = Vector3.zero;
         }
 
         private void Update()
         {
+            Deceleration();
         }
         #endregion
 
@@ -57,7 +70,52 @@ namespace AvatarController
         #region Private Methods
         private void OnMovement(Vector2 moveInput)
         {
+            Vector3 forward = CalculateForward();
+            Vector3 right = CalculateRight();
 
+            Vector3 movement = Vector3.zero;
+
+            movement = right * moveInput.x;
+            movement += forward * moveInput.y;
+
+            movement.Normalize();
+            AcceleratedMovement(movement);
+        }
+
+        private Vector3 CalculateForward()
+        {
+            Vector3 forward = CurrentCamera.transform.forward;
+            forward.y = 0;
+            forward.Normalize();
+            return forward;
+        }
+
+        private Vector3 CalculateRight()
+        {
+            Vector3 right = CurrentCamera.transform.right;
+            right.y = 0;
+            right.Normalize();
+            return right;
+        }
+
+
+        private void AcceleratedMovement(Vector3 movement)
+        {
+            Vector3 motion;
+
+            if(_velocity.magnitude < Data.DefaultMovement.MaxSpeed)
+            {
+                _velocity += Time.deltaTime * Data.DefaultMovement.Acceleration * movement;
+            }
+
+            motion = Time.deltaTime * _velocity;
+
+            _characterController.Move(motion);
+        }
+
+        private void Deceleration()
+        {
+            _velocity -= Time.deltaTime * Data.DefaultMovement.LinearDecceleration * (_velocity.normalized);
         }
         #endregion
     }
