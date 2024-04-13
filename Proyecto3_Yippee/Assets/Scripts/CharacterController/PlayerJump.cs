ced5b95b2aa14ee510c4035fbc57768ab07ccd6e
DarkAlejoxD <clondono@edu.tecnocampus.cat>
2024-04-13 16:30:47 +0200

    sasxa

--- Proyecto3_Yippee/Assets/Scripts/CharacterController/PlayerJump.cs
+++ Proyecto3_Yippee/Assets/Scripts/CharacterController/PlayerJump.cs
@@ -1,38 +1,242 @@
 ﻿using UnityEngine;
-using InputController;
+using AvatarController.Data;
+using UtilsComplements.Editor;
+using System;
 
 namespace AvatarController
 {
-    [RequireComponent(typeof(PlayerController))]
+    [RequireComponent(typeof(PlayerController), typeof(CharacterController))]
     public class PlayerJump : MonoBehaviour
     {
         //TODO: Get Design Specifications: For proto regular jump, for >=alpha mantein to higher
         //TODO: Make this class.
 
         #region Fields
+        [Header("References")]
         private PlayerController _controller;
+        private CharacterController _characterController;
+
+        [Header("Contro")]
+        private float _velocityY;
+        private bool _onGround;
+        private float _lastTimeInGround;
+        private bool _jumped;
+
+        private PlayerData DataContainer => _controller.DataContainer;
+        private float Gravity => Physics.gravity.y;
         #endregion
 
         #region Unity Logic
         private void Awake()
         {
+            _characterController = GetComponent<CharacterController>();
         }
 
-        private void Update()
+        private void OnEnable()
         {
+            if (_controller == null)
+                _controller = GetComponent<PlayerController>();
+
+            _controller.OnJump += OnJump;
+        }
+
+        private void OnDisable()
+        {
+            if (_controller == null)
+                _controller = GetComponent<PlayerController>();
+
+            _controller.OnJump -= OnJump;
         }
         #endregion
 
         #region Public Methods
-        public void PublicMethod()
+
+        private void Update()
         {
+            UpdateVy();
         }
         #endregion
 
         #region Private Methods
+        private void UpdateVy()
+        {
+            float variation = _velocityY * Time.deltaTime;
+
+            CollisionFlags movement = _characterController.Move(new Vector3(0, variation, 0));
+
+            if (movement == (CollisionFlags.Above))
+            {
+                _velocityY = 0;
+            }
+
+            if (movement == CollisionFlags.Below)
+            {
+                _onGround = true;
+                _lastTimeInGround = Time.time;
+                _jumped = false;
+                _velocityY = 0;
+            }
+            else
+            {
+                _onGround = false;
+            }
+
+            _velocityY += Gravity * Time.deltaTime;
+        }
+
         private void OnJump(bool active)
         {
+            if (!active)
+                return;
+
+            if (!CanJump())
+                return;
+
+            Jump();
         }
+
+        private bool CanJump()
+        {
+            if (_onGround)
+                return true;
+
+            if (_jumped)
+                return false;
+
+            if (Time.time <= _lastTimeInGround + DataContainer.DefaultJumpValues.CoyoteTime)
+                return true;
+
+            return false;
+        }
+
+        private void Jump()
+        {
+            _velocityY = GetVelocity();
+            _jumped = true;
+
+            #region DEBUG
+#if UNITY_EDITOR
+            lastPos = transform.position;
+            lastDir = transform.forward;
+#endif
+            #endregion
+        }
+
+        private float GetVelocity()
+        {
+            //VelocityCalculus
+            //v^2 - (v0)^2 = 2 * a * Dx
+            //V^2 = 0; v0 = ?; a = gravity; Dx = Data
+            //v0 = maths.sqrt ( -2 * a * Dx )
+
+            float vel = -2 * Gravity * DataContainer.DefaultJumpValues.MaxHeight;
+            return Mathf.Sqrt(Mathf.Abs(vel));
+
+
+            //OtherVelocityCalculus
+            //v = v0 + gt
+            //0 = v0 + gt
+            //v0 = -gt
+
+            //y = y0 + v0 * t + a/2 * t^2
+            //y=Data; y0 = 0; v0=? 
+            //return -Gravity * DataContainer.DefaultJumpValues.TimeToReachHeight;
+        }
+        #endregion
+
+        #region DEBUG
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            if (_controller == null)
+            {
+                _controller = GetComponent<PlayerController>();
+            }
+
+            DrawHeight();
+            DrawCurve();
+            DrawCoyoteCurve();
+        }        
+
+        private void DrawHeight()
+        {
+            GizmosUtilities.DrawSphereProperties sphereProperties =
+                new(GizmosUtilities.DrawSphereProperties.DefaultProperty)
+                {
+                    Radius = 0.3f
+                };
+
+            Vector3 height = lastPos + Vector3.up * DataContainer.DefaultJumpValues.MaxHeight;
+            Color color = Color.green;
+            GizmosUtilities.DrawSphere(height, color, sphereProperties,
+                                       DataContainer.DefaultJumpValues.DEBUG_drawHeight);
+        }
+
+        #region Curve
+        Vector3 lastPos = Vector3.zero;
+        Vector3 lastDir = Vector3.zero;
+
+        private void DrawCurve()
+        {
+            if (Application.isPlaying)
+            {
+                if (lastPos == Vector3.zero)
+                    lastPos = transform.position;
+                if (lastDir == Vector3.zero)
+                    lastDir = transform.forward;
+            }
+            else
+            {
+                lastDir = transform.forward;
+                lastPos = transform.position;
+            }
+
+            Color color = Color.blue;
+
+            GizmosUtilities.DrawCurveProperties curveProperties =
+                new(GizmosUtilities.DrawCurveProperties.DefaultValues)
+                {
+                    MinValue = 0,
+                    MaxValue = DataContainer.DefaultJumpValues.TimeToReachHeight * 2,
+                    DefinitionOfCurve = DataContainer.DefaultJumpValues.DEBUG_definitionOfJump
+                };
+
+            GizmosUtilities.DrawCurve(Curve, color, curveProperties,
+                                      DataContainer.DefaultJumpValues.DEBUG_drawCurve);
+        }
+
+        private void DrawCoyoteCurve()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private Vector3 Curve(float time)
+        {
+            Vector3 xzPos = Vector3.zero;
+            Vector3 yPos = Vector3.zero;
+
+            #region Y Axis
+            float vel = GetVelocity();
+
+            float y = lastPos.y + vel * time + Gravity / 2 * time * time;
+            yPos = new(0, y, 0);
+            #endregion
+
+            #region XY Axis
+            float speed = DataContainer.DefaultMovement.MaxSpeed *
+                          DataContainer.DefaultJumpValues.DEBUG_forwardMovementPct;
+
+            Vector3 direction = lastDir;
+            Vector3 pos = lastPos;
+
+            xzPos = pos + speed * time * direction;
+            #endregion
+
+            return xzPos + yPos;
+        }
+
+        #endregion
+#endif
         #endregion
     }
 }
---
+++ Proyecto3_Yippee/Assets/Scripts/Complements/GizmosUtilities.cs
@@ -0,0 +1,155 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace UtilsComplements.Editor
+{
+    public static class GizmosUtilities
+    {
+        #region DrawSphere
+        public struct DrawSphereProperties
+        {
+            public float Radius { get; set; }
+
+            public static DrawSphereProperties DefaultProperty { get; private set; }
+
+            static DrawSphereProperties()
+            {
+                DefaultProperty = new DrawSphereProperties()
+                {
+                    Radius = 1
+                };
+            }
+
+            public DrawSphereProperties(DrawSphereProperties copy)
+            {
+                Radius = copy.Radius;
+            }
+        }
+
+        public static void DrawSphere(Vector3 origin, bool drawControl = true)
+        {
+            DrawSphere(origin, Color.white, drawControl);
+        }
+
+        public static void DrawSphere(Vector3 origin, Color gizmosColor, bool drawControl = true)
+        {
+            DrawSphere(origin, gizmosColor, DrawSphereProperties.DefaultProperty, drawControl);
+        }
+
+        public static void DrawSphere(Vector3 origin, Color gizmosColor, DrawSphereProperties properties,
+                                      bool drawControl = true)
+        {
+            if (!drawControl)
+                return;
+
+            Gizmos.color = gizmosColor;
+            Gizmos.DrawWireSphere(origin, properties.Radius);
+            Gizmos.color = Color.white;
+        }
+        #endregion
+
+        #region DrawCurve
+
+        public delegate Vector3 Function(float time);
+        //public delegate Vector3 Function<T>(T parameters);
+
+        public struct DrawCurveProperties
+        {
+            public float MinValue { get; set; }
+            public float MaxValue { get; set; }
+            public int DefinitionOfCurve { get; set; }
+            public bool DrawDots { get; set; }
+            public float DotRadius { get; set; }
+            public int DottedLinesSpace { get; set; }
+
+            public static DrawCurveProperties DefaultValues { get; private set; }
+
+            static DrawCurveProperties()
+            {
+                DefaultValues = new DrawCurveProperties()
+                {
+                    MinValue = 0,
+                    MaxValue = 1,
+                    DefinitionOfCurve = 5,
+                    DottedLinesSpace = 1,
+                    DotRadius = 0.1f,
+                    DrawDots = true
+                };
+            }
+
+            public DrawCurveProperties(DrawCurveProperties copy)
+            {
+                MinValue = copy.MinValue;
+                MaxValue = copy.MaxValue;
+                DefinitionOfCurve = copy.DefinitionOfCurve;
+                DottedLinesSpace = copy.DottedLinesSpace;
+                DotRadius = copy.DotRadius;
+                DrawDots = copy.DrawDots;
+            }
+        }
+
+        public static void DrawCurve(Function function, bool drawControl = true)
+        {
+            DrawCurve(function, DrawCurveProperties.DefaultValues, drawControl);
+        }
+
+        public static void DrawCurve(Function function, DrawCurveProperties properties,
+                                     bool drawControl = true)
+        {
+            DrawCurve(function, Color.white, properties, drawControl);
+        }
+
+        public static void DrawCurve(Function function, Color gizmosColor,
+                                     DrawCurveProperties properties, bool drawControl = true)
+        {
+            if (!drawControl)
+                return;
+
+#if UNITY_EDITOR
+            Gizmos.color = gizmosColor;
+            Handles.color = gizmosColor;
+            static void resetColors()
+            {
+                Gizmos.color = Color.white;
+                Handles.color = Color.white;
+            }
+
+            #region Curve
+
+            int divisions = properties.DefinitionOfCurve;
+            if (divisions <= 1)
+            {
+                resetColors();
+                return;
+            }
+            Vector3 lastPos = Vector3.zero;
+            for (int i = 0; i <= divisions; i++)
+            {
+                float evaluatedValue = Mathf.Lerp(properties.MinValue, properties.MaxValue,
+                                                  ((float)i) / divisions);
+
+                Vector3 pos = function(evaluatedValue);
+                if (i != 0)
+                {
+                    Handles.DrawDottedLine(lastPos, pos, properties.DottedLinesSpace);
+                }
+                if (properties.DrawDots)
+                {
+                    Gizmos.DrawWireSphere(pos, properties.DotRadius);
+                }
+
+                lastPos = pos;
+            }
+
+            #endregion
+
+            resetColors();
+#endif
+        }
+
+        #endregion
+    }
+
+}
