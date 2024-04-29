93c5f62e9a1165b5f1379f8846eb12bd0154e84f
DarkAlejoxD <clondono@edu.tecnocampus.cat>
2024-04-29 19:16:55 +0200

    aaa

---
+++ Proyecto3_Yippee/Assets/Scripts/Complements/UtilMethods.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UtilsComplements
+{
+    public static class UtilMethods
+    {
+#if UNITY_EDITOR
+        [MenuItem(itemName: "Utils/Put Mesh Collider To Every MeshRenderer", isValidateFunction: false, priority = 1)]
+        private static void MeshRendererToCollider()
+        {
+            var list = GameObject.FindObjectsOfType<MeshRenderer>();
+
+            foreach (var item in list)
+            {
+                if (!item.TryGetComponent<MeshCollider>(out var a))
+                {
+                    item.gameObject.AddComponent<MeshCollider>();
+                }
+            }
+        }
+
+        [MenuItem(itemName: "Utils/Check Parity MeshCollider to Renderer", isValidateFunction: false, priority = 1)]
+        private static void CheckParityMeshColliderRenderer()
+        {
+            var list = GameObject.FindObjectsOfType<MeshCollider>();
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                var item = list[i];
+                if (!item.TryGetComponent<MeshRenderer>(out var a))
+                {
+                    GameObject.Destroy(item);
+                }
+            }
+        }
+#endif
+    }
+}
