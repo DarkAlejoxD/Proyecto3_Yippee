40a45cdab6c235fa4f99309e8676daa3f655a57d
DarkAlejoxD <clondono@edu.tecnocampus.cat>
2024-06-04 19:35:21 +0200

    Smoke particles

--- Proyecto3_Yippee/Assets/Scripts/CharacterController/Animations/PlayerAnimationEvents.cs
+++ Proyecto3_Yippee/Assets/Scripts/CharacterController/Animations/PlayerAnimationEvents.cs
@@ -4,15 +4,36 @@ namespace AvatarController.Animations
 {
     public class PlayerAnimationEvents : MonoBehaviour
     {
+        [Header("Player References")]
+        [SerializeField] private PlayerController _player;
+
         [Header("Step References")]
         [SerializeField] private ParticleSystem _stepsSmoke;
+        [SerializeField, Range(0, 1)] private float _stepProbability = 0.7f;
+        [SerializeField, Range(0, 1)] private float _threshold = 0.1f;
 
         [Header("Jump References")]
         [SerializeField] private ParticleSystem _jumpSmoke;
 
         public void Step()
         {
-            _stepsSmoke.Play();
+            float current = _player.Velocity.magnitude;
+            float minSpeed = _player.DataContainer.DefaultMovement.MinSpeedToMove;
+            float maxSpeed = _player.DataContainer.DefaultMovement.MaxSpeed;
+            float minSpeedPct = Mathf.Lerp(minSpeed, maxSpeed, _threshold);
+
+            if (current > minSpeedPct)
+                _stepsSmoke.Play();
+            else
+            {
+                _stepsSmoke.Stop();
+                return;
+            }
+
+            float rnd = Random.value;
+            if (rnd < _stepProbability)
+                _stepsSmoke.Emit(1);
+            //_stepsSmoke.Play();
             Debug.Log("Step SoundAndParticles");
         }
 
