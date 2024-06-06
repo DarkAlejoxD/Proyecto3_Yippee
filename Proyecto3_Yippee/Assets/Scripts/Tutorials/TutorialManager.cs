dc79083d6b51520ffaeecf5a953d602b79a50817
DarkAlejoxD <clondono@edu.tecnocampus.cat>
2024-06-06 21:42:46 +0200

    Tutorials finished, not implemented

--- Proyecto3_Yippee/Assets/Scripts/Tutorials/TutorialManager.cs
+++ Proyecto3_Yippee/Assets/Scripts/Tutorials/TutorialManager.cs
@@ -41,15 +41,19 @@ namespace Tutorials
         [SerializeField, Tooltip("Selection")] private GameObject _polter2KeyRef;
         [SerializeField, Tooltip("Selection")] private GameObject _polter2ConRef;
         [SerializeField, Tooltip("Movement")] private GameObject _polter3KeyRef;
-        [SerializeField, Tooltip("Movement")] private GameObject _polter3ConRef;
         private bool _polterMode = false;
         private int _controlIndex = 0;
+
+        [Header("ChangeAttributes")]
+        [Min(0.1f)] private float _changeCD = 1;
+        private float _timeControl;
 
         private List<GameObject> _allTutorials;
         private bool _isAppearing = false;
 
         private Menus _menuInputs;
         private ControllerStyle _controlStyle = ControllerStyle.Keyboard;
+        private ControllerStyle _lastStyle = ControllerStyle.Keyboard;
 
         public ISingleton<TutorialManager> Instance => this;
         #endregion
@@ -61,6 +65,7 @@ namespace Tutorials
             InputSystem.onDeviceChange += CheckControllerStyleUpdate;
             _keyboardPanelRef.SetActive(false);
             _controllerPanelRef.SetActive(false);
+            _timeControl = Time.time;
 
             _menuInputs = new();
             _menuInputs.Tutorials.Enable();
@@ -85,7 +90,6 @@ namespace Tutorials
             _allTutorials.Add(_polter2KeyRef);
             _allTutorials.Add(_polter2ConRef);
             _allTutorials.Add(_polter3KeyRef);
-            _allTutorials.Add(_polter3ConRef);
 
             Deactivate();
         }
@@ -97,6 +101,7 @@ namespace Tutorials
                 return;
 
             CheckTriggerUpdate();
+            ChangeInputStyle();
         }
 
         private void OnDestroy()
@@ -168,8 +173,8 @@ namespace Tutorials
                     case 1:
                         _controlIndex = 2;
 
-                        _polter1KeyRef.gameObject.SetActive(false);
-                        _polter1ConRef.gameObject.SetActive(false);
+                        _polter1KeyRef.SetActive(false);
+                        _polter1ConRef.SetActive(false);
 
                         _polter2KeyRef.SetActive(true);
                         _polter2ConRef.SetActive(true);
@@ -181,8 +186,10 @@ namespace Tutorials
                         _polter2KeyRef.gameObject.SetActive(false);
                         _polter2ConRef.gameObject.SetActive(false);
 
-                        _polter3KeyRef.SetActive(true);
-                        _polter3ConRef.SetActive(true);
+                        if (_controlStyle == ControllerStyle.Keyboard)
+                            _polter3KeyRef.SetActive(true);
+                        else
+                            Deactivate();
                         break;
                     case 3:
                         Deactivate();
@@ -199,7 +206,61 @@ namespace Tutorials
             if (!_isAppearing)
                 return;
 
-            
+            switch (change)
+            {
+                case InputDeviceChange.Enabled:
+                    {
+                        if (inputDevice is Gamepad)
+                        {
+                            _controlStyle = ControllerStyle.Gamepad;
+                            return;
+                        }
+
+                        if (inputDevice is Keyboard)
+                        {
+                            _controlStyle = ControllerStyle.Keyboard;
+                            return;
+                        }
+
+                        if (inputDevice is Mouse)
+                        {
+                            _controlStyle = ControllerStyle.Keyboard;
+                            return;
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void ChangeInputStyle()
+        {
+            if (Time.time <= _timeControl + _changeCD)
+                return;
+
+            switch (_controlStyle)
+            {
+                case ControllerStyle.Keyboard:
+                    if (_lastStyle == ControllerStyle.Gamepad)
+                    {
+                        _timeControl = Time.time;
+                        _lastStyle = ControllerStyle.Keyboard;
+
+                        _controllerPanelRef.SetActive(false);
+                        _keyboardPanelRef.SetActive(true);
+                    }
+                    break;
+                case ControllerStyle.Gamepad:
+                    if (_lastStyle == ControllerStyle.Keyboard)
+                    {
+                        _timeControl = Time.time;
+                        _lastStyle = ControllerStyle.Gamepad;
+                        _keyboardPanelRef.SetActive(false);
+                        _controllerPanelRef.SetActive(true);
+                    }
+                    break;
+            }
         }
         #endregion
     }
