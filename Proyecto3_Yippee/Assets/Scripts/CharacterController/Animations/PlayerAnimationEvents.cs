de8d636b4530e259169875024b0f65202d5990a8
DarkAlejoxD <clondono@edu.tecnocampus.cat>
2024-06-07 13:17:09 +0200

    Jump particles

--- Proyecto3_Yippee/Assets/Scripts/CharacterController/Animations/PlayerAnimationEvents.cs
+++ Proyecto3_Yippee/Assets/Scripts/CharacterController/Animations/PlayerAnimationEvents.cs
@@ -12,10 +12,19 @@ namespace AvatarController.Animations
         [Header("Step References")]
         [SerializeField] private ParticleSystem _stepsSmoke;
         [SerializeField, Range(0, 1)] private float _stepProbability = 0.7f;
-        [SerializeField, Range(0, 1)] private float _threshold = 0.1f;
+        [SerializeField, Range(0, 1)] private float _stepSpeedThreshold = 0.01f;
 
         [Header("Jump References")]
         [SerializeField] private ParticleSystem _jumpSmoke;
+        [SerializeField] private Transform _jumpPivot;
+        [SerializeField, Min(1)] private int _jumpParticlesCount = 50;
+
+        #region Unity Logic
+        private void Awake()
+        {
+            _jumpSmoke.transform.SetParent(null, true);
+        }
+        #endregion
 
         #region Public Methods
         public void Step()
@@ -23,7 +32,7 @@ namespace AvatarController.Animations
             float current = _player.Velocity.magnitude;
             float minSpeed = _player.DataContainer.DefaultMovement.MinSpeedToMove;
             float maxSpeed = _player.DataContainer.DefaultMovement.MaxSpeed;
-            float minSpeedPct = Mathf.Lerp(minSpeed, maxSpeed, _threshold);
+            float minSpeedPct = Mathf.Lerp(minSpeed, maxSpeed, _stepSpeedThreshold);
 
             if (current > minSpeedPct)
             {
@@ -45,7 +54,9 @@ namespace AvatarController.Animations
 
         public void Jump()
         {
+            _jumpSmoke.transform.position = _jumpPivot.position;
             PlayOneShot(Database.Player, "JUMP", transform.position);
+            _jumpSmoke.Emit(_jumpParticlesCount);
         }
 
         public void FallHit()
