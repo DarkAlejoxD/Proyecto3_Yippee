8b265776013ba5520635424c0ac793bff1b62505
DarkAlejoxD <clondono@edu.tecnocampus.cat>
2024-06-10 01:02:39 +0200

    dsad

---
+++ Proyecto3_Yippee/Assets/Scripts/BaseGame/ChangeScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace BaseGame
+{
+    public class ChangeScene : MonoBehaviour
+    {
+        [SerializeField, Min(0.1f)] private float _delayTime = 0.1f;
+        [SerializeField, Range(0, 2)] private int _sceneIndex = 0;
+        [SerializeField, TextArea(3, 4)]
+        private string _scenes =
+            "0: Menu" +
+            "\n1: Game" +
+            "\n2:Credits";
+
+        public void LoadScene()
+        {
+            SceneManager.LoadSceneAsync(_sceneIndex);
+        }
+    }
+}
