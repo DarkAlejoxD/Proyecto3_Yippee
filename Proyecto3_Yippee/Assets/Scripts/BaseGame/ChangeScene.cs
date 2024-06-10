ffa4b20f4169252f1dcc73e2cf20e45f9ae4b8c2
DarkAlejoxD <clondono@edu.tecnocampus.cat>
2024-06-10 02:09:57 +0200

    dsad

--- Proyecto3_Yippee/Assets/Scripts/BaseGame/ChangeScene.cs
+++ Proyecto3_Yippee/Assets/Scripts/BaseGame/ChangeScene.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using static UtilsComplements.AsyncTimer;
 
 namespace BaseGame
 {
@@ -7,13 +8,25 @@ namespace BaseGame
     {
         [SerializeField, Min(0.1f)] private float _delayTime = 0.1f;
         [SerializeField, Range(0, 2)] private int _sceneIndex = 0;
+        [SerializeField] private bool _playOnStart = false;
         [SerializeField, TextArea(3, 4)]
         private string _scenes =
             "0: Menu" +
             "\n1: Game" +
             "\n2:Credits";
 
+        private void Start()
+        {
+            if (_playOnStart)
+                LoadScene();
+        }
+
         public void LoadScene()
+        {
+            StartCoroutine(TimerCoroutine(_delayTime, Change));
+        }
+
+        private void Change()
         {
             SceneManager.LoadSceneAsync(_sceneIndex);
         }
